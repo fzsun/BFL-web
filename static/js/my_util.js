98fd1628fcf5b1c1169eac2d35084fcdd3ffca1f
fzsun <sunfangzhou1991@gmail.com>
2018-08-20 19:58:57 -0400

    Finished switching to Bulma, has the same function as boostrap now.

--- static/js/my_util.js
+++ static/js/my_util.js
@@ -1,28 +1,39 @@
-$(document).ready(function() {
-	$("form").on("submit", function(event) {
+// for navbar burger animation
+(function() {
+	var burger = document.querySelector('.burger');
+	var nav = document.querySelector('#'+burger.dataset.target);
+	burger.addEventListener('click', function(){
+		burger.classList.toggle('is-active');
+		nav.classList.toggle('is-active');
+	});
+})();
+
+// listen on form submit and reset
+$(document).ready(function () {
+	$("form").on("submit", function (event) {
 		event.preventDefault(); // prevent submit button from default post
-	  try {
-		$.post( $(location).attr('href') + "action/", // post url
-				$(this).serialize(),                  // post data
-				function(data,status) {               // process the post response
-				  localStorage.setItem( $(location).attr('pathname').replace(/\//g, '') + "result",
-										JSON.stringify(data));
-					if(typeof display_summary ==='function'){
-						display_summary();	// Note: this function will be customized in each page
+		try {
+			$.post($(location).attr('href') + "action/", // post url
+				$(this).serialize(), // post data
+				function (data, status) { // process the post response
+					localStorage.setItem($(location).attr('pathname').replace(/\//g, '') + "result",
+						JSON.stringify(data));
+					if (typeof display_summary === 'function') {
+						display_summary(); // Note: this function will be customized in each page
 					}
 				});
-	  } catch(err) {
-		alert(err);
-	  }
-	});
-	$("form").on( "reset", function(event) {
-	  try {
-			localStorage.removeItem($(location).attr('pathname').replace(/\//g, '') + "result")
-			if(typeof display_summary ==='function'){
-				display_summary();	// Note: this function will be customized in each page
-			}
-	  } catch(err) {
+		} catch (err) {
 			alert(err);
-	  }
+		}
+	});
+	$("form").on("reset", function (event) {
+		try {
+			localStorage.removeItem($(location).attr('pathname').replace(/\//g, '') + "result")
+			if (typeof display_summary === 'function') {
+				display_summary(); // Note: this function will be customized in each page
+			}
+		} catch (err) {
+			alert(err);
+		}
 	});
 });
