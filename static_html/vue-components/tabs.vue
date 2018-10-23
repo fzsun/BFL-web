b5f36f130014f15a01758a6da06dc51b4a096ee9
Robert <robes98@vt.edu>
2018-10-23 12:40:50 -0400

    Upload component

---
+++ static_html/vue-components/tabs.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+    <div class="tabs">
+        <ul>
+            <li v-for="tab in tabs" v-bind:class="{'is-active': tab.isActive}" :key="tab">
+            <a @click="selectTab(tab)">
+                {{ tab.name }}
+            </a>
+            </li>
+        </ul>
+    </div>
+    <div class="tab-details">
+        <slot></slot>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        tabs: [],
+        }
+    },
+    created() {
+        this.tabs = this.$children;
+    },
+    methods: {
+        selectTab(selectedTab) {
+        this.tabs.forEach(
+        function(tab){
+            tab.isActive = (selectedTab.name == tab.name)
+        }
+        ) 
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
