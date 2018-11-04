035a2fd2561ce185e370949911c0fada90865048
Robert <robes98@vt.edu>
2018-11-04 16:26:38 -0500

    Refactored code and pulled from output file

--- simulation/sim.py
+++ simulation/sim.py
@@ -10,94 +10,83 @@ import json
 import numpy as np
 
 
-with open("sim_example_data.json") as file: 
-    data = json.load(file)
+# open input and output files to be used
+with open("sim_example_data.json") as input_file: 
+    input_data = json.load(input_file)
+with open("example_output.json") as output_file: 
+    output_data = json.load(output_file)
+    
+# assign variables from input file
 SIM_TIME = 1040
-num_harvest_trucks = data['harvest_input']['num_trucks_harvest']
-carry_capacity = data['harvest_input']['carry_capacity']
-truck_speed_high = data['harvest_input']['truck_speed_high']
-truck_speed_low = data['harvest_input']['truck_speed_low']
-harvest_time = data['harvest_input']['harvest_time']
-farm_distance = data['harvest_input']['farm_distance']        
-farm_capacitys = data['harvest_input']['farm_capacity']
-harvest_progress = data['harvest_input']['harvest_progress']
-ssl_capacitys = data['ensilation_input']['SSL']
-num_trucks_road = data['road_input']['num_trucks_road']
-truck_capacity = data['road_input']['truck_capacity']
-rtruck_speed_high = data['road_input']['rtruck_speed_high']
-rtruck_speed_low = data['road_input']['rtruck_speed_low']
-ssl_distance = data['road_input']['ssl_distance']
+num_harvest_trucks = input_data['harvest_input']['num_trucks_harvest']
+carry_capacity = input_data['harvest_input']['carry_capacity']
+truck_speed_high = input_data['harvest_input']['truck_speed_high']
+truck_speed_low = input_data['harvest_input']['truck_speed_low']
+harvest_time = input_data['harvest_input']['harvest_time']  
+num_trucks_road = input_data['road_input']['num_trucks_road']
+truck_capacity = input_data['road_input']['truck_capacity']
+rtruck_speed_high = input_data['road_input']['rtruck_speed_high']
+rtruck_speed_low = input_data['road_input']['rtruck_speed_low']
+
+# assign variables from output file
+coord_f = output_data['params']['Coord_f'] # coordinates x,y for farms (all possible)
+coord_s = output_data['params']['Coord_s'] # coordinates x,y for ssl (all possible)
+K = output_data['params']['K'] # ssl size and equipment loadout configurations
+a = np.array(output_data['params']['a']) # 26 harvest schedule (yield for each farm in each period Mg)
+d = output_data['params']['d'] # demand
+u = output_data['params']['u'] # capacities for ssl
+ue = output_data['params']['ue'] # processing rate of non-chopping methods
+
+print(coord_f["3"][0])  #this is how we access
 
 ssl = []
 farms = []
-            
-def harvest(env, farms, period_totals, harvest_trucks, refinery, carry_capacity):
+m = a.shape[0]
+n = a.shape[1]
+
+print(n, " ",m)
+
+# create functions      
+def harvest(env, farms, a, m, n, harvest_trucks, refinery, carry_capacity):
     print(farms)
-    print(period_totals)
-    i=0
-    j=0
-    z=0
-    for period in period_totals:
-        for farm_yield in period:
-            farms[j].put(np.random.normal(period_totals[i][j], 1/100*period_totals[i][j]))
-            print('Total amount in farm: ', j, ' ', farms[j].level)
-            j=j+1
-        j=0
-        i=i+1
-        yield env.process(transport_harvest(env, farms, harvest_trucks, refinery, carry_capacity))
-        yield env.timeout(80)
+    
+    print(n)
+    for row in range(1,14):
+        for col in range(n):
+            farms[col].put(np.random.normal(a[row][col], 1/100*a[row][col]))
+            print('Total amount in farm: ', col, ' ', farms[col].level)
+            env.process(transport_harvest(env, farms[col], harvest_trucks, refinery, carry_capacity))
+        yield env.timeout(40)
         print('Current time', env.now)
 
 def ensilation():
     pass
    
-
-def transport_harvest(env, farms, harvest_trucks, refinery, carry_capacity):
-    for farm in farms:
-        while farm.level>carry_capacity:
-            with harvest_trucks.request() as req:
-                yield req
-                farm.get(carry_capacity)
-                #mylog(env, '23 Mg loaded to truck')
-                print('Number of trucks busy: ', harvest_trucks.count)
-                env.timeout(1)
-                refinery.put(carry_capacity)  
+def transport_harvest(env, farm, harvest_trucks, refinery, carry_capacity):
+    while farm.level>carry_capacity:
+        with harvest_trucks.request() as req:
+            yield req
+            farm.get(carry_capacity)
+            print('Number of trucks busy: ', harvest_trucks.count)
+            env.timeout(1)
+            refinery.put(carry_capacity)  
     print('Amount in refinery: ', refinery.level)
        
 
 def mylog(env, string):
    print(f'@{env.now}: {harvest_trucks.count} busy harvest tucks, ssl inventory={ssl.level}, {trucks.count} busy trucks, refinery inventory={refinery.level}| {string}')
 
+# Define containers and resources and start process
 env = simpy.Environment()
 
-for ssl_capacity in ssl_capacitys:
-   ssl.append(simpy.Container(env, ssl_capacity, init=0))
-for farm_capacity in farm_capacitys:
-    farms.append(simpy.Container(env, farm_capacity, init=0))
+# create farms
+for farm in range(n):
+    farms.append(simpy.Container(env, 10000000, init=0))
+    
 harvest_trucks = simpy.Resource(env, capacity=num_harvest_trucks)
 road_trucks = simpy.Resource(env, capacity=num_trucks_road)
 refinery = simpy.Container(env, capacity=100000000, init=0)
-period_totals = np.zeros(shape=(len(harvest_progress),len(farms)))
-i=0
-j=0
-#intialize harvest calendar
-for row in period_totals:
-    for col in row:
-        period_totals[i][j] = farm_capacitys[j]*(harvest_progress[i]/100)
-        j=j+1
-    j=0
-    i=i+1  
-demand = 0
-for farm in farms:
-    demand = demand + farm.capacity
-#start process
-env.process(harvest(env, farms, period_totals, harvest_trucks, refinery, carry_capacity))
+
+env.process(harvest(env, farms, a, m, n, harvest_trucks, refinery, carry_capacity))
 env.run(until=SIM_TIME)
-
-if refinery.level >= demand:
-    print('Demand was met for your planning horizon')
-if refinery.level <= demand:
-    actual = 100-refinery.level/demand
-    print(actual, '% of your demand for the planning horizon was met' )
-
 
