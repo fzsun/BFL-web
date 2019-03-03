5d0bf43c29beee5b2c2714e2e1bca0efb304df7b
Benjamin Hadinger <bmh.bfl@gmail.com>
2019-03-03 14:47:41 -0500

    Finished with api

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -1,23 +1,37 @@
 <template>
   <body>
     <div id="map"></div>
+    <p id="choice">Current choice: Farm</p>
+    <form id="changeType">
+        <a href="#" v-on:click="farm_or_ssl">CHANGE</a>
+    </form>
+    </br>
+    </br>
     <form id="refineryFormLatLong">
-        <label>Refinery Location</label>
+        <label>Location</label>
         <input id="farmnameLatLon" type="text" placeholder="Farm">
         <input id="latInput" type="number" placeholder="Lat">
         <input id="lonInput" type="number" placeholder="Lng">
         <a href="#" v-on:click="latlon">SUBMIT</a>
     </form>
+    </br>
+    </br>
     <form id="refineryFormAddress">
-        <label>Refinery Location</label>
-        <input id="farmnameLatLon" type="text" placeholder="Farm">
+        <label>Location</label>
+        <input id="farmnameAddress" type="text" placeholder="Farm">
         <input id="address" type="text" placeholder="Address">
         <a href="#" v-on:click="address">SUBMIT</a>
     </form>
+    </br>
+    </br>
     <form id="getLocations">
-        <a href="#" v-on:click="locations">SUBMIT</a>
+        <a href="#" v-on:click="locations">Print Locations</a>
     </form>
     <p id="locations"></p>
+    </br>
+    <form id="submitLocations">
+        <a href="#" v-on:click="submitLocations">Send Locations</a>
+    </form>
   </body>
 </template>
 
@@ -28,20 +42,37 @@ export default {
     name: 'Google Maps',
     data() {
         return {
+            //0 = farm, 1 = ssl, 2 = refinery
+            type: 0,
+			name: 'Farm',
             msg: 'BFL Map',
-            centerLat: 0,
-            centerLon: 0,
+            refinery: null,
+            refineryMarker: null,
             farmsCounter: 0,
+            sslCounter: 0,
             farms: [],
-            storages: [],
-            markers: []
+            ssls: [],
+            farmMarkers: [],
+            sslMarkers: []
     }},
     methods: {
+        farm_or_ssl : function() {
+            this.type = (this.type + 1) % 3;
+			
+			if (this.type == 2) this.name = "Refinery"
+            else if (this.type == 1) this.name = "SSL";
+            else this.name = "Farm";
+           	
+			document.getElementById("choice").innerHTML = "Current choice: " + this.name;
+            document.getElementById("farmnameLatLon").placeholder = this.name;
+            document.getElementById("farmnameAddress").placeholder = this.name;
+        },
+
         //Initial retrieval of map
         getMap(){
             this.map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 61, lng: -149},
-            zoom: 4
+            center: {lat: 37.22904237824045, lng: -80.41982042804534},
+            zoom: 16
             })
         },
 
@@ -49,38 +80,91 @@ export default {
         addPushpin() {
             var ref = this;
             google.maps.event.addListener(this.map, 'click', function(event) {
-                 var farmname = prompt("Farm Name", "Farm");
-                 ref.placeMarker(event.latLng, farmname);
+            	var typeName = prompt(ref.name + " Name", this.name);
+                ref.placeMarker(event.latLng, typeName);
             });
         },
-
-        placeMarker(location, farmname) {
+        placeMarker(location, locationname) {
             var ref = this;
-            var marker = new google.maps.Marker({
-                position: location,
-                map: this.map,
-                title: farmname
-            });
-            var id = this.farmsCounter;
-            google.maps.event.addListener(marker, "rightclick", 
-                function (point) { ref.delMarker(id) });
+			if (this.type == 2) {
+				if (this.refinery != null) {
+					this.refineryMarker.setMap(null);
+				}
+				var marker = new google.maps.Marker({
+                    position: location,
+                    map: this.map,
+                    title: name,
+                    icon: {
+                        url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+                    }
+                });
+				google.maps.event.addListener(marker, "rightclick",
+                    function (point) { ref.delMarker(0, "refinery") });
+				this.refinery = {name: locationname,
+                                 latitude: location.lat(),
+                                 longitude: location.lng()};
+				this.refineryMarker = marker;
+            } else if (this.type == 1) {
+                var marker = new google.maps.Marker({
+                    position: location,
+                    map: this.map,
+                    title: name,
+                    icon: {
+                        url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+                    }
+                });
 
-            this.markers[this.farmsCounter] = marker;
-            this.farms[this.farmsCounter] = {name: farmname,
+                var id = this.sslCounter;
+                google.maps.event.addListener(marker, "rightclick",
+                    function (point) { ref.delMarker(id, "ssl") });
+
+                this.sslMarkers[this.sslCounter] = marker;
+                this.ssls[this.sslCounter] = {name: locationname,
                                              latitude: location.lat(),
-                                             longitude: location.lng()}
-            this.farmsCounter = this.farmsCounter + 1;
+                                             longitude: location.lng()};
+                this.sslCounter = this.sslCounter + 1;
+            } else {
+                var marker = new google.maps.Marker({
+                    position: location,
+                    map: this.map,
+                    title: name,
+                    icon: {
+                        url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
+                    }
+                });
+
+                var id = this.farmsCounter;
+                google.maps.event.addListener(marker, "rightclick",
+                    function (point) { ref.delMarker(id, "farm") });
+
+                this.farmMarkers[this.farmsCounter] = marker;
+                this.farms[this.farmsCounter] = {name: locationname,
+                                                 latitude: location.lat(),
+                                                 longitude: location.lng()};
+                this.farmsCounter = this.farmsCounter + 1;
+            }
+        },
+        delMarker(id, type) {
+			if (type == "refinery") {
+				var marker = this.refineryMarker;
+				marker.setMap(null);
+				this.refinery = null;
+				this.refineryMarker = null;
+            } else if (type == "ssl") {
+                var marker = this.sslMarkers[id];
+                marker.setMap(null);
+                this.ssls[id] = null;
+                this.sslMarkers[id] = null;
+            } else {
+                var marker = this.farmMarkers[id];
+                marker.setMap(null);
+                this.farms[id] = null;
+                this.farmMarkers[id] = null;
+            }
         },
 
-        delMarker(id) {
-            var marker = this.markers[id]; 
-            marker.setMap(null);
-            this.farms[id] = null;
-            this.markers[id] = null;
-        },
 
-
-        //Submit Forms for farm locations
+        //Submit Forms for farm locations (by lat,lng and by address)
         latlon : function(){
             var lat = document.getElementById("latInput").value;
             var lon = document.getElementById("lonInput").value;
@@ -88,13 +172,13 @@ export default {
             var myLatlng = new google.maps.LatLng(lat, lon);
             this.placeMarker(myLatlng, farmname);
         },
-        address : function(){
+        address : function() {
             var address = document.getElementById("address").value;
-            var farmname = document.getElementById("farmnameLatLon").value;
-            var url = 'https://api.geocod.io/v1.3/geocode?' + 
+            var farmname = document.getElementById("farmnameAddress").value;
+            var url = 'https://api.geocod.io/v1.3/geocode?' +
                       'q=' + address +
                       '&api_key=' + '57cf5c27cf057777f7fd555f33f3b56d77f5da5';
- 
+
             var result = axios
                 .get(url)
                 .then(response => {
@@ -114,9 +198,8 @@ export default {
             });
         },
 
-
-        //Get Locations
-        locations : function(){
+        //Print Locations to website
+        locations : function() {
             var locations = "Locations: </br>";
             var k;
             for(k = 0; k < this.farmsCounter; k++) {
@@ -127,7 +210,40 @@ export default {
                                 this.farms[k].longitude + "</br>";
                 }
             }
+            locations = locations + "SSL Locations: </br>";
+            for(k = 0; k < this.sslCounter; k++) {
+                if (this.ssls[k] != null) {
+                    locations = locations +
+                                this.ssls[k].name + ": " +
+                                this.ssls[k].latitude + " -- " +
+                                this.ssls[k].longitude + "</br>";
+                }
+            }
+			locations = locations + "Refinery: </br>";
+			if (this.refinery != null) {
+				locations = locations +
+              	            this.refinery.name + ": " +
+                            this.refinery.latitude + " -- " +
+                            this.refinery.longitude + "</br>";
+			}
             document.getElementById("locations").innerHTML = locations;
+        },
+
+        //Submit locations to background for optimization
+        submitLocations : function() {
+            var filteredFarm = this.farms.filter(function (el) {
+                return el != null;
+            });
+            var filteredSSL = this.ssls.filter(function (el) {
+                return el != null;
+            });
+            console.log("submitting locations");
+            console.log(filteredFarm);
+            console.log(filteredSSL);
+            console.log(this.refinery.name);
+
+//            axios
+//                .post('http://localhost:5000/geo/',
         }
     },
     mounted: function() {
